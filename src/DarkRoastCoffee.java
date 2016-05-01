7348f3c6d48367eae23cd2070f3efb32c3befa27
Nebojsa <nebojsa.stanic86@gmail.com>
2016-05-01 11:29:17 +0200

    Initial commit.

---
+++ src/DarkRoastCoffee.java
@@ -0,0 +1,12 @@
+
+public class DarkRoastCoffee extends Beverage{
+	
+	public DarkRoastCoffee() {
+		description = "Dark Roast Coffee";
+	}
+	@Override
+	public double cost() {
+		return 0.99;
+	}
+
+}
---
+++ src/HouseBlendCoffee.java
@@ -0,0 +1,12 @@
+
+public class HouseBlendCoffee extends  Beverage{
+	
+	public HouseBlendCoffee() {
+		description = "House Blend Coffee";
+	}
+	@Override
+	public double cost() {
+		return 0.89;
+	}
+
+}
